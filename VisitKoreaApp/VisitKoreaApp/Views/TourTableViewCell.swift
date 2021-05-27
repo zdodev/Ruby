069d1bb171dfa01f50e2b5dddf91b9e0bddbba37
zdodev <iver0945@gmail.com>
2021-05-28 00:51:58 +0900

    ⚡️: 테이블 뷰 셀 데이터를 설정하는 configureCell 메서드 생성.

--- VisitKoreaApp/VisitKoreaApp/Views/TourTableViewCell.swift
+++ VisitKoreaApp/VisitKoreaApp/Views/TourTableViewCell.swift
@@ -2,5 +2,17 @@ import UIKit
 
 final class TourTableViewCell: UITableViewCell {
     @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var inquiringLabel: UILabel!
     
+    func configureCell(_ tourViewModel: TourViewModel) {
+        titleLabel.text = tourViewModel.title
+        addressLabel.text = tourViewModel.address
+        inquiringLabel.text = tourViewModel.inquiringNumber
+    }
+    
+    func configureCellImage() {
+        
+    }
 }
