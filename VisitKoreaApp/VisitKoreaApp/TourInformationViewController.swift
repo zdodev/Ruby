6bc9572dc06acaefa12322d0febacaa8ad0abc6d
zdodev <iver0945@gmail.com>
2021-05-28 00:55:14 +0900

    ⚡️: 데이터 소스 구현 및 응답 데이터 테이블 뷰에 표현.

--- VisitKoreaApp/VisitKoreaApp/TourInformationViewController.swift
+++ VisitKoreaApp/VisitKoreaApp/TourInformationViewController.swift
@@ -2,22 +2,58 @@ import UIKit
 
 final class TourInformationViewController: UIViewController {
     @IBOutlet weak var tourListTableView: UITableView!
+    private let tourAPIService = TourAPIService(sessionManager: URLSession.shared)
+    private var tourViewModels = [TourViewModel]()
+    private var page = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tourListTableView.dataSource = self
+        queryTourInformation()
+        queryTourInformation()
+        queryTourInformation()
+    }
+    
+    func queryTourInformation() {
+        tourAPIService.search(pageNumber: page) { [weak self] result in
+            switch result {
+            case .success(let tourInformation):
+                guard let tourViewModel = self?.makeTourViewModel(tourInformation) else {
+                    return
+                }
+                self?.tourViewModels.append(tourViewModel)
+                DispatchQueue.main.async {
+                    self?.tourListTableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func makeTourViewModel(_ tourInformation: TourInformation) -> TourViewModel {
+        let title = tourInformation.response.body.items.item.title
+        var address = tourInformation.response.body.items.item.baseAddress
+        if let detailAddress = tourInformation.response.body.items.item.detailAddress {
+            address += detailAddress
+        }
+        let inquiringNumber = tourInformation.response.body.items.item.inquiringNumber
+        
+        return TourViewModel(title: title, address: address, inquiringNumber: inquiringNumber)
     }
 }
 
 extension TourInformationViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        tourViewModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tourListTableView.dequeueReusableCell(withIdentifier: "TourTableViewCell", for: indexPath) as! TourTableViewCell
-        cell.mainImage.backgroundColor = .systemBlue
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TourTableViewCell", for: indexPath) as? TourTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configureCell(tourViewModels[indexPath.row])
         return cell
     }
 }
