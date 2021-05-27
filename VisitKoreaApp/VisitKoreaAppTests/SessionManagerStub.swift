bb1862822c8dac32d2f5ee2bf62394f7ef2f6963
zdodev <iver0945@gmail.com>
2021-05-27 22:59:55 +0900

    ⚡️: SessionManagerStub 스텁 타입 생성.

---
+++ VisitKoreaApp/VisitKoreaAppTests/SessionManagerStub.swift
@@ -0,0 +1,29 @@
+import Foundation
+@testable import VisitKoreaApp
+
+final class SessionManagerStub: SessionManagerProtocol {
+    var url: URL?
+    
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        self.url = url
+        
+        let stubData = makeStubData()
+        
+        completionHandler(stubData, nil, nil)
+        
+        return URLSession.shared.dataTask(with: URLRequest(url: url))
+    }
+    
+    private func makeStubData() -> Data? {
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: "TourInformationListSample", withExtension: "json") else {
+            return nil
+        }
+        
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            return nil
+        }
+    }
+}
