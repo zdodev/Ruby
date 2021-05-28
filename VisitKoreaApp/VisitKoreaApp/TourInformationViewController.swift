5c01996de24c19c2734dd460aa4c8eb817c73b06
zdodev <iver0945@gmail.com>
2021-05-28 13:51:23 +0900

    ⚡️: 페이징 구현 및 단일 아이템 요청 메서드 구현.

--- VisitKoreaApp/VisitKoreaApp/TourInformationViewController.swift
+++ VisitKoreaApp/VisitKoreaApp/TourInformationViewController.swift
@@ -4,27 +4,30 @@ final class TourInformationViewController: UIViewController {
     @IBOutlet weak var tourListTableView: UITableView!
     private let tourAPIService = TourAPIService(sessionManager: URLSession.shared)
     private var tourViewModels = [TourViewModel]()
-    private var page = 1
+    private var isPaging = false
+    private var pageNumber = 0
+    private var totalPage = 0
+    private var remainPage = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tourListTableView.dataSource = self
-        queryTourInformation()
-        queryTourInformation()
-        queryTourInformation()
     }
     
-    func queryTourInformation() {
+    private func queryTourInformation(_ page: Int) {
         tourAPIService.search(pageNumber: page) { [weak self] result in
             switch result {
             case .success(let tourInformation):
+                let totalPage = tourInformation.response.body.totalCount
+                let currentPage = tourInformation.response.body.numberOfRows * tourInformation.response.body.pageNumber
+                self?.totalPage = totalPage
+                self?.remainPage = totalPage - currentPage
                 guard let tourViewModel = self?.makeTourViewModel(tourInformation) else {
                     return
                 }
-                self?.tourViewModels.append(tourViewModel)
+                self?.tourViewModels.append(contentsOf: tourViewModel)
                 DispatchQueue.main.async {
                     self?.tourListTableView.reloadData()
+                    self?.isPaging = false
                 }
             case .failure(let error):
                 print(error)
@@ -32,15 +35,52 @@ final class TourInformationViewController: UIViewController {
         }
     }
     
-    private func makeTourViewModel(_ tourInformation: TourInformation) -> TourViewModel {
-        let title = tourInformation.response.body.items.item.title
-        var address = tourInformation.response.body.items.item.baseAddress
-        if let detailAddress = tourInformation.response.body.items.item.detailAddress {
-            address += detailAddress
+    private func queryTourInformationSingleItem(_ page: Int) {
+        tourAPIService.searchSingleItem(pageNumber: page) { [weak self] result in
+            switch result {
+            case .success(let tourInformationSingleItem):
+                let totalPage = tourInformationSingleItem.response.body.totalCount
+                let currentPage = tourInformationSingleItem.response.body.numberOfRows * tourInformationSingleItem.response.body.pageNumber
+                self?.totalPage = totalPage
+                self?.remainPage = totalPage - currentPage
+                self?.totalPage = tourInformationSingleItem.response.body.totalCount
+                guard let tourViewModel = self?.makeTourSingleViewModel(tourInformationSingleItem) else {
+                    return
+                }
+                self?.tourViewModels.append(tourViewModel)
+                DispatchQueue.main.async {
+                    self?.tourListTableView.reloadData()
+                    self?.isPaging = false
+                }
+            case .failure(let error):
+                print(error)
+            }
         }
-        let inquiringNumber = tourInformation.response.body.items.item.inquiringNumber
-        
-        return TourViewModel(title: title, address: address, inquiringNumber: inquiringNumber)
+    }
+    
+    private func nextPaging() {
+        pageNumber += 1
+        isPaging = true
+        if remainPage == 1 {
+            queryTourInformationSingleItem(pageNumber)
+        } else {
+            queryTourInformation(pageNumber)
+        }
+    }
+    
+    private func makeTourViewModel(_ tourInformation: TourInformation) -> [TourViewModel] {
+        tourInformation.response.body.items.item.map { item in
+            TourViewModel(title: item.title,
+                          address: item.baseAddress + (item.detailAddress ?? "") ,
+                          inquiringNumber: item.inquiringNumber)
+        }
+    }
+    
+    private func makeTourSingleViewModel(_ tourInformationSingleItem: TourInformationSingleItem) -> TourViewModel {
+        TourViewModel(title: tourInformationSingleItem.response.body.items.item.title,
+                      address: tourInformationSingleItem.response.body.items.item.baseAddress +
+                        (tourInformationSingleItem.response.body.items.item.detailAddress ?? ""),
+                      inquiringNumber: tourInformationSingleItem.response.body.items.item.inquiringNumber)
     }
 }
 
@@ -55,5 +95,19 @@ extension TourInformationViewController: UITableViewDataSource {
         }
         cell.configureCell(tourViewModels[indexPath.row])
         return cell
+    }
+}
+
+extension TourInformationViewController: UITableViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.height
+        
+        if offsetY > (contentHeight - height) {
+            if isPaging == false && remainPage > 0 {
+                nextPaging()
+            }
+        }
     }
 }
