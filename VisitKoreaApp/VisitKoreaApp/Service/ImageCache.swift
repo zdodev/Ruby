a500ea8614cb9b7527d3dc3b3560392bcd5073b2
zdodev <iver0945@gmail.com>
2021-05-29 18:01:13 +0900

    ⚡️: 이미지 디스크캐시 기능 구현.

--- VisitKoreaApp/VisitKoreaApp/Service/ImageCache.swift
+++ VisitKoreaApp/VisitKoreaApp/Service/ImageCache.swift
@@ -4,9 +4,48 @@ struct ImageCache {
     private let fileManager = FileManager.default
     private let imageMemoryCache = NSCache<NSString, NSData>()
     private let tourImageService = TourImageService(sessionManager: URLSession.shared)
+    private let path: URL
+    
+    init() {
+        path = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+    }
 
     func fetchImage(urlString: String, cell: TourTableViewCell) {
-        downloadImage(urlString, cell)
+        guard let fileName = separateLastPathComponent(urlString) else {
+            return
+        }
+        
+        if isDiskCached(fileName) {
+            guard let cachedData = fetchDiskCache(fileName) else {
+                return
+            }
+            cell.configureCellImage(data: cachedData)
+        } else {
+            downloadImage(urlString, cell)
+        }
+    }
+    
+    private func isDiskCached(_ fileName: String) -> Bool {
+        let filePath = path.appendingPathComponent(fileName)
+        return fileManager.fileExists(atPath: filePath.path)
+    }
+    
+    private func fetchDiskCache(_ fileName: String) -> Data? {
+        let filePathURL = path.appendingPathComponent(fileName)
+        do {
+            let cachedData = try Data(contentsOf: filePathURL)
+            return cachedData
+        } catch {
+            return nil
+        }
+    }
+
+    private func createDiskCache(_ urlString: String, _ data: Data) {
+        guard let fileName = separateLastPathComponent(urlString) else {
+            return
+        }
+        let filePath = path.appendingPathComponent(fileName)
+        fileManager.createFile(atPath: filePath.path, contents: data)
     }
     
     private func downloadImage(_ urlString: String, _ cell: TourTableViewCell) {
@@ -14,9 +53,14 @@ struct ImageCache {
             switch result {
             case .success(let data):
                 cell.configureCellImage(data: data)
+                createDiskCache(urlString, data)
             case .failure(let error):
                 print(error)
             }
         }
+    }
+    
+    private func separateLastPathComponent(_ urlString: String) -> String? {
+        URL(string: urlString)?.lastPathComponent
     }
 }
