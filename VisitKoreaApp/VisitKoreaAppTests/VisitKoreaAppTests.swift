ef74780eee93f4c3e6bed311095c402fd52f2c2c
zdodev <iver0945@gmail.com>
2021-05-30 12:52:02 +0900

    🧪: JSONAnalyzer 디코딩 성공 테스트 작성.

--- VisitKoreaApp/VisitKoreaAppTests/VisitKoreaAppTests.swift
+++ VisitKoreaApp/VisitKoreaAppTests/VisitKoreaAppTests.swift
@@ -2,48 +2,26 @@ import XCTest
 @testable import VisitKoreaApp
 
 class VisitKoreaAppTests: XCTestCase {
-    func test_관광API_JSON_디코딩_성공() {
+    func test_관광API_TourInformationJSON_디코딩_성공() {
         let sut = JSONAnalyzer()
         let bundle = Bundle(for: type(of: self))
-        guard let url = bundle.url(forResource: "TourInformationListSample", withExtension: "json") else {
-            XCTFail()
+        guard let url = bundle.url(forResource: "TourInformationSample", withExtension: "json") else {
+            XCTFail("TourInformationSample 리소스를 찾을 수 없습니다.")
             return
         }
         var data = Data()
         do {
             data = try Data(contentsOf: url)
         } catch {
-            XCTFail()
+            XCTFail("TourInformationSample 리소스를 읽을 수 없습니다.")
         }
         
-        guard let tourData = sut.decodeJSON(TourInformation.self, data: data) else {
-            XCTFail()
+        guard let decodedData = sut.decodeJSON(TourInformation.self, data: data) else {
+            XCTFail("TourInformationSample 리소스를 디코딩할 수 없습니다.")
             return
         }
         
-        let expectedValue = "OK"
-        XCTAssertEqual(tourData.response.header.resultMsg, expectedValue)
-    }
-    
-    func test_pageNumber_파라미터를_전달하여_search_메서드_호출() {
-        let sessionManagerStub = SessionManagerStub()
-        let sut = TourAPIService(sessionManager: sessionManagerStub)
-        
-        sut.search(pageNumber: 1) { _ in }
-        
-        guard let url = sessionManagerStub.url else {
-            XCTFail()
-            return
-        }
-        let expectedURL = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?serviceKey=hm3Ng%2Bp0hajUH1lyqqB1JTmURPuIidiOj%2BoR1I49TQDEJPB9eY9CrArmUXrlx1PQ1DqvA%2B%2FqNSJWJhFa73mamw%3D%3D&numOfRows=1&MobileApp=AppTest&MobileOS=ETC&arrange=A&contentTypeId=15&areaCode=4&sigunguCode=&listYN=Y&_type=json&pageNo=1"
-        XCTAssertEqual(url.absoluteString, expectedURL)
-        sut.search(pageNumber: 1) { result in
-            switch result {
-            case .success(let data):
-                XCTAssertEqual(data.response.header.resultMsg, "OK")
-            case .failure(_):
-                XCTFail()
-            }
-        }
+        let expectedValue = "대구광역시 북구 호암로 15"
+        XCTAssertEqual(decodedData.response.body.items.item[0].baseAddress, expectedValue)
     }
 }
