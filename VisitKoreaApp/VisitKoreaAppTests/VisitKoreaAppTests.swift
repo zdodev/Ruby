9842aab210a9d6e86d442e112d041af64063bb47
zdodev <iver0945@gmail.com>
2021-05-30 12:57:41 +0900

    🧪: 잘못된 데이터 디코딩 실패 테스트.

--- VisitKoreaApp/VisitKoreaAppTests/VisitKoreaAppTests.swift
+++ VisitKoreaApp/VisitKoreaAppTests/VisitKoreaAppTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import VisitKoreaApp
 
 class VisitKoreaAppTests: XCTestCase {
-    func test_관광API_TourInformationJSON_디코딩_성공() {
+    func test_관광API_TourInformationJSON_디코딩을_성공() {
         let sut = JSONAnalyzer()
         let bundle = Bundle(for: type(of: self))
         guard let url = bundle.url(forResource: "TourInformationSample", withExtension: "json") else {
@@ -23,5 +23,14 @@ class VisitKoreaAppTests: XCTestCase {
         
         let expectedValue = "대구광역시 북구 호암로 15"
         XCTAssertEqual(decodedData.response.body.items.item[0].baseAddress, expectedValue)
+    }
+    
+    func test_관광API_잘못된_JSON데이터_디코딩을_실패() {
+        let sut = JSONAnalyzer()
+        let wrongData = Data()
+        
+        let decodedData = sut.decodeJSON(TourInformation.self, data: wrongData)
+        
+        XCTAssertNil(decodedData, "잘못된 데이터를 디코딩했습니다.")
     }
 }
